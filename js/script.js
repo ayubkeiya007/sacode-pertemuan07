95d82840abaa0f2ae7cc3168e08a2b85ab5a47b8
ayub keiya <ayubkeiya007@gmail.com>
2024-08-29 20:01:18 +0900

    simpan fail

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// single line comment
+
+/*
+multiple line commant
+bisa tulisjuga 
+*/
+
+
+var namaDepan = "ayub";
+var namaBelakang = "keiya"
+
+document.getElementById("nama")
+.innerHTML = namaDepan + namaBelakang
+
